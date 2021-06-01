be3cd5240a321307e85cbee6c209fe4d45087461
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-06-01 23:14:59 +0530

    LunchTtime Sol.

--- CodeChef_May_LucnchTime_Solution_2021/Charges.cpp
+++ CodeChef_May_LucnchTime_Solution_2021/Charges.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 #define ll long long int
 
-int main(){
+int main()
+{
 
-    int n,k;
+    int n, k;
     cin >> n >> k;
 
     string s;
     cin >> s;
 
-    int len = n-1;
-    for(ll i = 1; i < n; i++){
-        if(s[i] == s[i-1]){
+    int len = n - 1;
+    for (ll i = 1; i < n; i++)
+    {
+        if (s[i] == s[i - 1])
+        {
             len += 1;
         }
     }
@@ -22,33 +25,42 @@ int main(){
     {
         /* code */
         ll q;
-        cin >>q;
+        cin >> q;
         q--;
 
-        if(s[q] == '1'){
+        if (s[q] == '1')
+        {
             s[q] = '0';
-        }else{
+        }
+        else
+        {
             s[q] = '1';
         }
 
-        if(q < n-1){
-            if(s[q] == s[q+1]){
+        if (q < n - 1)
+        {
+            if (s[q] == s[q + 1])
+            {
                 len++;
-            }else{
+            }
+            else
+            {
                 len--;
             }
         }
 
-        if(q > 0){
-            if(s[q] == s[q-1]){
+        if (q > 0)
+        {
+            if (s[q] == s[q - 1])
+            {
                 len--;
-            }else{
+            }
+            else
+            {
                 len--;
             }
         }
-        cout << len <<endl;
-
+        cout << len << endl;
     }
     return 0;
-    
 }
