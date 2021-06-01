89a43ae39ab07be153ae1aa0db21e8449a29df8f
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-06-01 23:14:48 +0530

    LunchTtime Sol.

---
+++ CodeChef_May_LucnchTime_Solution_2021/Charges.cpp
@@ -0,0 +1,54 @@
+// LunchTtime Sol.
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+
+    int n,k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+
+    int len = n-1;
+    for(ll i = 1; i < n; i++){
+        if(s[i] == s[i-1]){
+            len += 1;
+        }
+    }
+
+    while (k--)
+    {
+        /* code */
+        ll q;
+        cin >>q;
+        q--;
+
+        if(s[q] == '1'){
+            s[q] = '0';
+        }else{
+            s[q] = '1';
+        }
+
+        if(q < n-1){
+            if(s[q] == s[q+1]){
+                len++;
+            }else{
+                len--;
+            }
+        }
+
+        if(q > 0){
+            if(s[q] == s[q-1]){
+                len--;
+            }else{
+                len--;
+            }
+        }
+        cout << len <<endl;
+
+    }
+    return 0;
+    
+}
