48e4796b3d4a5e6e949d24562376d924d763a093
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-06-01 21:27:36 +0530

    // LunchTtime Sol.

--- CodeChef_May_LucnchTime_Solution_2021/Hoops.cpp
+++ CodeChef_May_LucnchTime_Solution_2021/Hoops.cpp
@@ -1,1 +1,15 @@
 // LunchTtime Sol.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    if (n % 2 != 0){
+        int sum = n / 2 + 1;
+        cout << sum <<endl;
+    }
+    return 0;
+}
