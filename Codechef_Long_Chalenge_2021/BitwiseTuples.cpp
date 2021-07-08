0ccf212c08ed20620dd366819470fe133d220322
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-08 17:01:00 +0530

    code

---
+++ Codechef_Long_Chalenge_2021/BitwiseTuples.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >>t;
+    int n,m;
+
+    while (t--)
+    {
+        /* code */
+        
+        cin >> n >>m;
+        int num = 1;
+        int arr[n];
+        for (int i = n; i < n ; i++)
+        {
+            /* code */
+            arr[i] = n;
+        }
+        
+        
+
+    }
+    
+}
